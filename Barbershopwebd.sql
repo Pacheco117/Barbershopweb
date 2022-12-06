c43c0b015bb1dc3fefc59546ea368764362e1c11
maxk1218 <66450526+maxk1218@users.noreply.github.com>
2022-12-06 16:58:11 -0600

    base de datos

--- Barbershopwebd.sql
+++ Barbershopwebd.sql
@@ -19,17 +19,44 @@
 CREATE DATABASE IF NOT EXISTS `barbershopweb` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `barbershopweb`;
 
+-- Volcando estructura para tabla barbershopweb.citas
+CREATE TABLE IF NOT EXISTS `citas` (
+  `N_Cita` int(11) NOT NULL AUTO_INCREMENT,
+  `Telefono` int(10) DEFAULT NULL,
+  `NServicio` int(11) DEFAULT NULL,
+  `ID_Hora` int(11) DEFAULT NULL,
+  `ID_Fecha` int(11) DEFAULT NULL,
+  `estadocitaFK` int(11) DEFAULT NULL,
+  PRIMARY KEY (`N_Cita`),
+  UNIQUE KEY `Telefono` (`Telefono`,`NServicio`,`ID_Hora`,`ID_Fecha`,`estadocitaFK`),
+  KEY `NServicio` (`NServicio`),
+  KEY `ID_Hora` (`ID_Hora`),
+  KEY `ID_Fecha` (`ID_Fecha`),
+  KEY `estadocitaFK` (`estadocitaFK`),
+  CONSTRAINT `citas_ibfk_1` FOREIGN KEY (`NServicio`) REFERENCES `servicios` (`Nservicio`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `citas_ibfk_2` FOREIGN KEY (`ID_Hora`) REFERENCES `horas` (`ID_Hora`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `citas_ibfk_3` FOREIGN KEY (`ID_Fecha`) REFERENCES `fechas` (`ID-Fecha`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `citas_ibfk_4` FOREIGN KEY (`Telefono`) REFERENCES `clientes` (`Telefono`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `citas_ibfk_5` FOREIGN KEY (`estadocitaFK`) REFERENCES `estadocita` (`ID-Est`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='Tabla que guarda las citas agendadas';
+
+-- Volcando datos para la tabla barbershopweb.citas: ~1 rows (aproximadamente)
+INSERT INTO `citas` (`N_Cita`, `Telefono`, `NServicio`, `ID_Hora`, `ID_Fecha`, `estadocitaFK`) VALUES
+	(1, 2147483647, 3, 18, 1, 1);
+
 -- Volcando estructura para tabla barbershopweb.clientes
 CREATE TABLE IF NOT EXISTS `clientes` (
-  `Telefono` int(11) NOT NULL,
+  `Telefono` int(10) NOT NULL,
   `Nombre` varchar(50) DEFAULT NULL,
   `ApellidoP` varchar(50) DEFAULT NULL,
   `ApellidoM` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`Telefono`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='La tabla que guarda los datos de nuestros clientes';
 
--- Volcando datos para la tabla barbershopweb.clientes: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla barbershopweb.clientes: ~3 rows (aproximadamente)
 INSERT INTO `clientes` (`Telefono`, `Nombre`, `ApellidoP`, `ApellidoM`) VALUES
+	(1234567890, 'juanito', 'prada', 'channel'),
+	(2123212321, 'pedrito', 'perez', 'nuñes'),
 	(2147483647, 'Juan', 'Juarez', 'Perez');
 
 -- Volcando estructura para tabla barbershopweb.estadocita
@@ -52,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `fechas` (
   PRIMARY KEY (`ID-Fecha`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='Aqui se guardan cada una de las fechas que se registran en la tabla de clientes';
 
--- Volcando datos para la tabla barbershopweb.fechas: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla barbershopweb.fechas: ~1 rows (aproximadamente)
 INSERT INTO `fechas` (`ID-Fecha`, `Fecha`) VALUES
 	(1, '2022/12/06');
 
