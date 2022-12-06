fca4915f14716d8cecf9c5f9210bb0efd475a671
Pacheco117 <sergio777.117@gmail.com>
2022-12-05 20:55:34 -0600

    colores del menu

--- app/Views/AgregarClientes.php
+++ app/Views/AgregarClientes.php
@@ -248,11 +248,7 @@ Administrador</h5>
             
             </div>
   <!-- Optional JavaScript -->
-  <script src="../assets/vendor/jquery/jquery-3.3.1.min.js"></script>
-    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
-    <script src="../assets/vendor/slimscroll/jquery.slimscroll.js"></script>
-    <script src="../assets/vendor/parsley/parsley.js"></script>
-    <script src="../assets/libs/js/main-js.js"></script>
+ 
     <script>
     $('#form').parsley();
     </script>
